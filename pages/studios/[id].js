b17e9670bef18bebacf2d43f5db97492270f219a
Vittorio Rivabella <iwasvitto@gmail.com>
2021-06-14 12:21:57 +0200

    removing studio and job pages

--- pages/studios/[id].js
+++ pages/studios/[id].js
@@ -1,72 +1,70 @@
-import client from "../api/graphql/apolloClient"
-import { GET_STUDIO_BY_ID, GET_ALL_STUDIOS_IDS, GET_JOBS_BY_STUDIO} from "../api/graphql/queries/queries"
-import StudioHeader from "../../components/StudioHeader"
-import JobListItem, { JobListContainer} from "../../components/partials/jobListItem"
+// import client from "../api/graphql/apolloClient"
+// import { GET_STUDIO_BY_ID, GET_ALL_STUDIOS_IDS, GET_JOBS_BY_STUDIO} from "../api/graphql/queries/queries"
+// import StudioHeader from "../../components/StudioHeader"
+// import JobListItem, { JobListContainer} from "../../components/partials/jobListItem"
 
-export const getStaticPaths = async () => {
-    const { data } = await client.query({query : GET_ALL_STUDIOS_IDS}) 
-    if(data){
-        
-    }
-    const paths = data.studios.map(studio => 
-        {
-            return {params : {id : studio.id.toString()}}
-        }
-    )
+// export const getStaticPaths = async () => {
+//     const { data } = await client.query({query : GET_ALL_STUDIOS_IDS}) 
 
-    return {
-        paths,
-        fallback: false,
-    }
+//     const paths = data.studios.map(studio => 
+//         {
+//             return {params : {id : studio.id.toString()}}
+//         }
+//     )
 
-}
+//     return {
+//         paths,
+//         fallback: false,
+//     }
+
+// }
 
 
 
-export const getStaticProps = async(context) => {
-    const studioId = context.params.id
-    console.log(studioId)
+// export const getStaticProps = async(context) => {
+//     const studioId = context.params.id
+//     console.log(studioId)
 
-    const { data }  = await client.query({
-        query: GET_STUDIO_BY_ID,
-        variables:{
-            id: parseInt(studioId)
-        }
-    })
-    // TODO: MIGHT BECOME ONE CALL
-    const jobs = await client.query({
-        query: GET_JOBS_BY_STUDIO,
-        variables: {
-            studioId : parseInt(studioId)
-        }
-    })
+//     const { data }  = await client.query({
+//         query: GET_STUDIO_BY_ID,
+//         variables:{
+//             id: parseInt(studioId)
+//         }
+//     })
+//     // TODO: MIGHT BECOME ONE CALL
+//     const jobs = await client.query({
+//         query: GET_JOBS_BY_STUDIO,
+//         variables: {
+//             studioId : parseInt(studioId)
+//         }
+//     })
 
-    console.log(data)
+//     console.log(data)
 
-    return{
-        props:{
-            studioName: data.studio.studio_name,
-            studioWebsite: data.studio.studio_website,
-            studioLogo: data.studio.studio_logo,
-            studioLocation: data.studio.studio_location,
-            jobs: jobs.data.jobs
-        }
-    }
-}
+//     return{
+//         props:{
+//             studioName: data.studio.studio_name,
+//             studioWebsite: data.studio.studio_website,
+//             studioLogo: data.studio.studio_logo,
+//             studioLocation: data.studio.studio_location,
+//             jobs: jobs.data.jobs
+//         }
+//     }
+// }
 
 
-export default function studioDetails({studioName, studioWebsite, studioLogo, jobs, studioLocation}){
-    console.log(jobs)
-    return (
-        <div>
-            <StudioHeader title = {studioName} logo = {studioLogo} location ={studioLocation}  ></StudioHeader>
+// export default function studioDetails({studioName, studioWebsite, studioLogo, jobs, studioLocation}){
+//     console.log(jobs)
+//     return (
+//         <div>
+//             <StudioHeader title = {studioName} logo = {studioLogo} location ={studioLocation}  ></StudioHeader>
 
-            <JobListContainer >
-            <p>Jobs found: {jobs.length}</p>
-            <ul>
-              {jobs && jobs.map(job => <JobListItem keyValue={job.id}  jobRole = {job.job_title} jobLocation = {job.job_location} studio = {studioName} studioLogo = {studioLogo} jobLocation = {job.job_location}  jobDescription = {job.job_description}></JobListItem>)}
-            </ul>
-          </JobListContainer>
-        </div>
-    )
-}
+//             <JobListContainer >
+//             <p>Jobs found: {jobs.length}</p>
+//             <ul>
+//               {jobs && jobs.map(job => <JobListItem keyValue={job.id}  jobRole = {job.job_title} jobLocation = {job.job_location} studio = {studioName} studioLogo = {studioLogo} jobLocation = {job.job_location}  jobDescription = {job.job_description}></JobListItem>)}
+//             </ul>
+//           </JobListContainer>
+//         </div>
+//     )
+// }
