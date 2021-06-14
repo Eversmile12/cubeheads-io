0272e0c294df5d9b30e43866f587b0320674c8bb
Vittorio Rivabella <iwasvitto@gmail.com>
2021-06-14 10:44:50 +0200

    removing cache

--- pages/api/graphql/apolloClient.js
+++ pages/api/graphql/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
     uri: "https://cubeheads.io/api/graphql",
-    cache: new InMemoryCache()
+    // cache: new InMemoryCache()
 })
 
 
