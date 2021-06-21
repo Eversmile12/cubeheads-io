fb74f35bd503542dd5a83b5edac067f1352f714e
Vittorio Rivabella <iwasvitto@gmail.com>
2021-06-21 10:30:13 +0200

    sorting redirect

--- pages/jobs/[id].js
+++ pages/jobs/[id].js
@@ -20,7 +20,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false
+        fallback: true
     }
 }
 
--- pages/studios/[id].js
+++ pages/studios/[id].js
@@ -16,7 +16,7 @@ export const getStaticPaths = async () => {
 
     return {
         paths,
-        fallback: false,
+        fallback: true,
     }
 
 }
